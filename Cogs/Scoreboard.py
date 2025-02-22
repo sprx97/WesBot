b32e87b6ad0f0098b1d5c3deadb4250ab48e615c
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2025-02-22 18:05:18 -0500

    Fix missing lines

--- Cogs/Scoreboard.py
+++ Cogs/Scoreboard.py
@@ -464,6 +464,10 @@ class Scoreboard(WesCog):
         if state not in ["LIVE", "CRIT", "OVER", "FINAL", "OFF"]:
             return
 
+        # TODO: Try to eliminate reliance on landing
+        landing = make_api_call(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/landing")
+        play_by_play = make_api_call(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play")
+
         # TODO: This works with either play_by_play or landing
         away, away_emoji, home, home_emoji = get_teams_from_landing(play_by_play)
 
@@ -471,10 +475,6 @@ class Scoreboard(WesCog):
         if game_id not in self.messages:
             # TODO: Remove "Goals" when changing over to play-by-play
             self.messages[game_id] = {"awayTeam": away, "homeTeam": home, "events": {}, "Goals": {}}
-
-        # TODO: Try to eliminate reliance on landing
-        landing = make_api_call(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/landing")
-        play_by_play = make_api_call(f"https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play")
 
         GATE_USE_PLAY_BY_PLAY = True
 
