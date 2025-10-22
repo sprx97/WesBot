87c8cf4dc78eb3e0ed9310099b7bba05441f88ee
Jeremy Vercillo <jeremy.vercillo@gmail.com>
2025-10-22 18:04:38 +0000

    Reactivate weekly inactives check

--- Cogs/OTH.py
+++ Cogs/OTH.py
@@ -25,7 +25,6 @@ class OTH(WesCog):
         self.trades_loop.start()
         self.loops.append(self.trades_loop)
 
-# Disabled for the summer
         self.inactives_loop.start()
         self.loops.append(self.inactives_loop)
 
@@ -222,7 +221,7 @@ class OTH(WesCog):
 
     # Checks all OTH leagues for inactive managers and abandoned teams
     async def check_inactives(self):
-        return # disabled for offseason. TODO: Add a weekvar check here once I get my shared config file
+        # return # disabled for offseason. TODO: Add a weekvar check here once I get my shared config file
 
         channel = self.bot.get_channel(MODS_CHANNEL_ID)
         leagues = get_leagues_from_database(Config.config["year"])
